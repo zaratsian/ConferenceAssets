62f0deab618c27f208476ea6f498cfbbd664a40c
Dan Zaratsian <dtz001@gmail.com>
2019-04-05 00:19:07 +0000

    updated model

--- score_automl_nlp.py
+++ score_automl_nlp.py
@@ -6,7 +6,7 @@
 '''
 USAGE:
 score_automl_nlp.py <project_id> <model_id> <news_headline>
-score_automl_nlp.py "zproject201807" "TCN4087840641364848832" "test headline"
+score_automl_nlp.py "zproject201807" "TCN1398431117116560846" "test headline"
 '''
 
 
@@ -29,7 +29,13 @@ if __name__ == '__main__':
     model_id   = sys.argv[2]
     content    = sys.argv[3] 
     
-    print(get_prediction(content, project_id,  model_id))
+    predictions = get_prediction(content, project_id,  model_id)
+    
+    print('') 
+    for i, result in enumerate(predictions.payload): 
+        print( '{}\t{}'.format( result.classification.score, result.display_name  ))
+    
+    print('')
 
 
 #ZEND
